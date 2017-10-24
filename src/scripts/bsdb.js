0a1d8b83707de1c3f345784554b5fecee8b47fc4
Nathaniel Wiley <nwiley86@gmail.com>
2017-10-24 09:05:07 -0400

    Add bsdb
    Stubbed out file. WIP

---
+++ src/scripts/bsdb.js
@@ -0,0 +1,32 @@
+;var bsdb = bsdb || (function(){
+	var APP = {
+		tbHeroes:{},// Hero records
+		tbFeatures:{},// Feature records
+		tbRates:{},// Rate records
+		tbLocs:{},// Location records
+		tbOrgs:{},// Organization records
+		lkO_H:{},// link Organizations to Heroes
+		lkH_R:{},// link Heroes to Rates
+		lkH_F:{},// link Heroes to Features
+		lkL_R:{},// link Locations to Rates
+		load:function(data){
+
+		},
+		addHero:function(){
+
+		},
+		addFeature:function(){
+
+		},
+		addRate:function(){
+
+		},
+		addLoc:function(){
+
+		},
+		addOrg:function(){
+
+		}
+	}
+	return APP;
+})();
